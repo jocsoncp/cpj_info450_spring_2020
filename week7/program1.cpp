f9eb4d4caa5cd27c17f5c2bc45517b035ca6f961
jocsoncp <59986912+jocsoncp@users.noreply.github.com>
2020-03-05 19:06:28 -0500

    commit

--- week7/program1.cpp
+++ week7/program1.cpp
@@ -9,7 +9,7 @@ int main()
     string a1; 
     cout << "Please enter a string.";
     getline(cin, a1);
-    reverse(a1);
+    string a1 = reverse(a1);
     cout << a1;
     return 0;
 }
@@ -17,8 +17,8 @@ int main()
 string reverse(string a)
 {
     string b = "";
-    int stop = a.size;
-    for (int go = 0; go < a.size; go++)
+    int stop = a.length();
+    for (int go = 0; go < a.length(); go++)
     {
         
         string b = b + a[stop];
