b2bf980d45a38e9e1189131320275d155316fcf2
jocsoncp <59986912+jocsoncp@users.noreply.github.com>
2020-04-15 23:54:59 -0400

    commit

--- week11/random_write.cpp
+++ week11/random_write.cpp
@@ -3,28 +3,22 @@
 #include <stdlib.h>
 #include <time.h> 
 using namespace std;
-int gen();
 int main()
 {
-    int go;
+    
     ofstream write("randoms.txt");
     if (!write)
     {
         cout << "Cannot open file" << endl;
         return 1;
     }
-    go = gen();
-    write << go << endl;
+    srand(time(NULL));
+    for (int x = 1000; x != 0; x--)
+    {
+        int num;
+        num = rand() % 20000 + -10000;
+        write << num << endl;
+    }
     write.close();
     return 0;
-}
-
-int gen()
-{
-    int num;
-    int count;
-    if (count = 1) return 1;
-    srand(time(NULL));
-    num = rand() % 20000 + -10000;
-    return num;
 }
